7d9db674ca615cd82219a815881e61f3fe48f814
ArieGonzAguer <103292223+diosDeNada@users.noreply.github.com>
2023-10-06 17:40:05 -0600

    segundo commit. archivos terminados

--- script.js
+++ script.js
@@ -23,21 +23,27 @@ const mostrarOnzas = document.getElementById("mostrarOnzas");
 mostrarPromedio.addEventListener("click", () => {
   let resultado = montoTotalPromedio.value / cantidadArticulos.value;
   resultadoPromedio.innerText = `Cada artículo te sale en ${resultado}`;
+  console.log(typeof resultado);
+  console.log(typeof montoTotalPromedio);
+  console.log(typeof montoTotalPromedio.value);
 })
 
 // Descuento
-mostrarDescuento.addEventListener("click", () => { //corregir esto. tira NaN
-  let resultado = (descuentoFinal / 100) * montoT;
-  resultadoDescuento.innerText = `El monto a descontar es: ${resultado}`;
-  console.log(resultado);
+mostrarDescuento.addEventListener("click", () => {
+  let resultado = (descuentoFinal.value / 100) * montoT.value;
+  let montoConDescuento = montoT.value - resultado;
+  resultadoDescuento.innerText = `El monto a descontar es: ${resultado}.\n El nuevo total a pagar es: ${montoConDescuento}`;
 })
 
 // Fahrenheit a Celsius
 mostrarCelsius.addEventListener("click", () => {
+  let resultado = (gradosFarenheit.value - 32) * (5 / 9);
+  resultadoCelsius.innerText = `${gradosFarenheit.value}°F es equivalente a ${resultado}°C.`
 
 })
 
 // Onzas a gramos
 mostrarOnzas.addEventListener("click", () => {
-
+  let resultado = onzas.value * 28.3495;
+  resultadoGramos.innerText = `${onzas.value} Oz es equivalente a ${resultado}g.`
 })
